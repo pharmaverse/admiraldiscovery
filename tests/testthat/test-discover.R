e8ceed4a142c0e559d685b646ba4520f67106733
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-11-20 08:49:39 -0800

    Update test-discover.R

--- tests/testthat/test-discover.R
+++ tests/testthat/test-discover.R
@@ -1,7 +1,7 @@
 test_that("Check Valid URLs", {
   valid_url <- function(url_in, t = 2) {
     con <- url(url_in)
-    check <- suppressWarnings(try(open.connection(con, open = "rt", timeout = t), silent = T)[1])
+    check <- suppressWarnings(try(open.connection(con, open = "rt", timeout = t), silent = TRUE)[1])
     suppressWarnings(try(close.connection(con), silent = TRUE))
     ifelse(is.null(check), TRUE, FALSE)
   }
