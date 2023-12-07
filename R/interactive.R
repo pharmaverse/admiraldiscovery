3b5ef726f23e83a141a3a80f61dea0a850994961
Amelia Tran <tran26h@mtholyoke.edu>
2023-12-06 21:32:43 -0800

    Add superseded and deprecated highlight to reactables (#32)
    * attempt to add Deprecation and Superseded highlights to reactable tables
    
    * Update interactive.R
    
    * added heading
    
    * add supersede/deprecate highlights to reactable
    
    * remove hard-coded statuses
    
    * add base case when no superseded and deprecated
    
    * Update DESCRIPTION
    
    * Update interactive.R
    
    ---------
    
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

--- R/interactive.R
+++ R/interactive.R
@@ -23,9 +23,26 @@ interactive_discovery <- function(type = c("gt", "reactable")) {
 }
 
 .reactable_discovery <- function() {
-  admiraldiscovery::discovery |>
-    dplyr::select(-c("package", "fn", "resource1_text")) |>
+  temp <- admiraldiscovery::discovery |>
+    dplyr::left_join(
+      get_admiral_deprecated() |> dplyr::mutate(deprecated = TRUE),
+      by = c("package", "fn")
+    ) |>
+    dplyr::left_join(
+      get_admiral_superseded() |> dplyr::mutate(superseded = TRUE),
+      by = c("package", "fn")
+    ) |>
+
+  dplyr::select(-c("package", "fn", "resource1_text"))
+
+  temp |>
     reactable::reactable(
+      rowStyle = function(index) {
+        if (is.na(temp[index, "superseded"]) && is.na(temp[index, "deprecated"])) list(background = '#ffffff')
+        else if (temp[index, "superseded"] == TRUE) list(background = '#ffffba')
+        else if (temp[index, "deprecated"] == TRUE) list(background = '#ffb3ba')
+      },
+
       columns = list(
         dataset = reactable::colDef(
           name = attr(admiraldiscovery::discovery$dataset, 'label'),
@@ -59,8 +76,11 @@ interactive_discovery <- function(type = c("gt", "reactable")) {
             )
           },
           width = 150
-        )
+        ),
+        deprecated = reactable::colDef(show = FALSE),
+        superseded = reactable::colDef(show = FALSE)
       ),
+
       searchable = TRUE,
       filterable = TRUE,
       defaultPageSize = 20,
@@ -79,17 +99,17 @@ interactive_discovery <- function(type = c("gt", "reactable")) {
       get_admiral_superseded() |> dplyr::mutate(superseded = TRUE),
       by = c("package", "fn")
     ) |>
-    dplyr::mutate(
-      function_link = glue::glue("[`{package}::{fn}()`]({fn_url})"),
-      .after = "fn_url"
-    ) |>
+  dplyr::mutate(
+    function_link = glue::glue("[`{package}::{fn}()`]({fn_url})"),
+    .after = "fn_url"
+  ) |>
     dplyr::mutate(
       resource1_link = glue::glue("[{resource1_text}]({resource1_url})")
     ) |>
     gt::gt() |>
     gt::cols_hide(columns = c("fn", "fn_url", "package", "dataset_type",
-                          "resource1_text", "resource1_url",
-                          "deprecated", "superseded")) |>
+                              "resource1_text", "resource1_url",
+                              "deprecated", "superseded")) |>
     gt::cols_label(
       function_link = "Function",
       resource1_link = "Resources"
@@ -107,7 +127,6 @@ interactive_discovery <- function(type = c("gt", "reactable")) {
     gt::sub_missing(missing_text = "")  |>
     gt::cols_width(c(dataset, dataset_type, variable) ~ gt::pct(8)) |>
     gt::cols_align(align = "left") |>
-    gt::tab_options(table.font.size = 11) |>
     gt::opt_interactive(
       use_search = TRUE,
       use_filters = TRUE,
