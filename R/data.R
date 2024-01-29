61e4e02202771331ebc326062a35c90673a5f8b8
Edoardo Mancini <53403957+manciniedoardo@users.noreply.github.com>
2024-01-29 16:10:13 +0000

    Closes #35 moved presentation archive to gt (#40)
    * #35 moved presentation archive to gt
    
    * Update DESCRIPTION
    
    * #35 removed interactivity, documentated data
    
    * #35 correct typo, document data
    
    ---------
    
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

--- R/data.R
+++ R/data.R
@@ -3,3 +3,9 @@
 #' This data set lists admiral functions and links them to ADaM data sets and
 #' variables they help create.
 "discovery"
+
+#' Presentation Archive
+#'
+#' This data set contains information and links to resources of recent presentations
+#' about admiral-related topics.
+"presentation_archive"
