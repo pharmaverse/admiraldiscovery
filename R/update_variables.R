5903f28c6c404f160b49daa6c77ece543bf80f6e
jimrothstein <jimrothstein@gmail.com>
2025-07-14 09:20:06 -0700

    DRAFT .... update with data from JSON

--- R/update_variables.R
+++ R/update_variables.R
@@ -1,45 +1,35 @@
 ## update_variables.R
 
 ## ------------------------  update_variables.R
-## STATUS:    Do not review;  this is file is placeholder
+## STATUS:    DRAFT.  If ok, will roxygenate, lint, standardize.... 
 ## GOAL:      Using JSON file in pharamaverseadam as source,
 ##            update the .csv file admiral-lookup-book.csv in admiraldiscovery.
-## Approach:  simple as possible.   Any benefit to `tibblify` package?
 ## ------------------------  
 
 
 library(jsonlite)
 library(pak)
-library(tibblify) # an option
+library(readr)
 
-## ------------------------  target
+## ------------------------  target (file to update)
 
-# URL
-## "https://github.com/pharmaverse/admiraldiscovery/blob/main/inst/admiral-lookup-book.csv" 
 
 csv_file  <- "https://raw.githubusercontent.com/pharmaverse/admiraldiscovery/refs/heads/main/inst/admiral-lookup-book.csv"
 
-target  <- read.csv(csv_file)
-dim(target)   #424 x 9
 
-# OR, 
-library(readr)
-target2  <- readr::read_csv(csv_file)
-
+target  <- readr::read_csv(csv_file)
 ## ------------------------  source
-
-
-## URL
-##"https://github.com/pharmaverse/pharmaverseadam/blob/main/inst/extdata/adams-specs.json"
 
 json_file <- "https://raw.githubusercontent.com/pharmaverse/pharmaverseadam/refs/heads/main/inst/extdata/adams-specs.json"
 
 source_json  <- fromJSON(json_file)
-source_json
-v  <- source_json$variables
-typeof(v)
-length(source_json$variables)
+source <- as_tibble(source_json$Variables)
+
+# Change keys in source to match the keys used target.
+new = dplyr::rename(source, dataset=Dataset, variable=Variable, variable_label = Label)
+new2 = new |> dplyr::select(dataset, variable, variable_label)
 
 
-source_tib  <- tibblify(json_source)
+# ------------------------  update
 
+res = dplyr::rows_update(x=target, y=new2, by = c("dataset", "variable"), unmatched = "ignore")
